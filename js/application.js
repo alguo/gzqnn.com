fb0134d8194213bfe0b2804dd8d6d97698475420
zackwong <hzzzoo@126.com>
2014-07-09 16:48:03 +0800

    up

---
+++ js/application.js
@@ -0,0 +1,8 @@
+//= require jquery-1.8.3.min.js
+//= require header.js
+//= require sliderkit-1.9.2.min.js
+//= require jquery-etalage-1.3.1.min.js
+//= require jquery.isotope.min.js
+//= require jquery.lazyload-1.8.4.min.js
+//= require qnnpano.js
+//= require thickbox.min.js
